ed94ed525bc792ed1f4fc32cf6cb1f2435394498
Kouji Sasaya <kouji.sasaya@hotmail.co.jp>
2026-03-31 23:24:49 +0900

    fix:

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,13 @@ set -e
 
 cd /workdir
 
+
+echo "Usage:"
+echo " cargo init    # Create new your urst package"
+echo " cargo build   $ Build Your rust packaghe"
+echo " cargo run     # Run your rust package"
+echo " cargo fmt     # Fortmat your rust code"
+
 if [ $# -gt 0 ]; then
     exec "$@"
 else
